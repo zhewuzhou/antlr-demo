a8942d9f8653ddda2e9d107f535642f98437a231
zhewuzhou <zhewuzhou@gmail.com>
2020-11-18 21:56:30 +0800

    [Z.Z][Property] properties file tree walker

---
+++ app/src/main/java/antlr/book/code/visitor/CollectPropsVisitor.java
@@ -0,0 +1,20 @@
+package antlr.book.code.visitor;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CollectPropsVisitor extends PropertyFileBaseVisitor<Void> {
+    Map<String, String> props = new TreeMap<>();
+
+    @Override
+    public Void visitProp(PropertyFileParser.PropContext ctx) {
+        String id = ctx.ID().getText();
+        String value = ctx.STRING().getText();
+        props.put(id, value);
+        return null;
+    }
+
+    public Map<String, String> getProps() {
+        return props;
+    }
+}
