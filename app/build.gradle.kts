0d3d4325459b2661c3a7bf1c2986e02ea3cda82c
zhewuzhou <zhewuzhou@gmail.com>
2020-11-20 11:00:00 +0800

    [Z.Z][Refactor] the formal way of apply kotlin in java project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.10"
 }
 
 repositories {
@@ -25,7 +25,7 @@ dependencies {
     val junitJupiterVersion = "5.6.0"
 
     // Align versions of all Kotlin components
-    platform("org.jetbrains.kotlin:kotlin-bom")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
 
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
